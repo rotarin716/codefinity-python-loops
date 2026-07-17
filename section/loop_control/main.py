c6e7b30e8acf204283fdd20545287bbbc1697a86
rotarin716 <>
2026-07-17 11:39:52 +0000

    Loop Control

---
+++ section/loop_control/main.py
@@ -0,0 +1,25 @@
+# List of countries you are considering for travel
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of countries that require a visa
+visa_required = ['China', 'India', 'Saudi Arabia', 'Brazil', 'United Arab Emirates', 'Egypt']
+
+# List of visa-free travel destinations
+travel_list = []
+
+count=0
+
+for country in countries:
+    if country in visa_required:
+        continue
+        
+    if count==10:
+        break
+    travel_list.append(country)
+    count+=1
+      
+        
+    
+
+# Testing
+print('Visa-free travel destinations:', travel_list)
